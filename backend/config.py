362799018dce5c6375cf00ec1529acf87b9b4d62
ahmadfantastic <ahmadfantastic.ads@gmail.com>
2025-05-17 16:28:09 -0400

    - fix typo in gitignore of venv path - remove instance from gitignore - add abitilty to to name database file from .env

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///project.db")
+    DB_NAME = os.getenv("DB_NAME", "default_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", f"sqlite:///{DB_NAME}.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OPENAI_DEFAULT_MODEL = os.getenv("OPENAI_DEFAULT_MODEL", "gpt-4o")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
     # Switch to another LLM provider by overriding *_API_KEY and in openai_client.py
